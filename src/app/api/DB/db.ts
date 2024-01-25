e2840fc5e796fba4cddea73a1b6f18a9373a7c36
borislav130606@gmail.com <borislav130606@gmail.com>
2024-01-25 19:56:19 +0300

    db hotfix

--- src/app/api/DB/db.ts
+++ src/app/api/DB/db.ts
@@ -1,13 +1,14 @@
 import * as pg from 'pg';
 import { Sequelize } from "sequelize";
 // 'postgres://postgres:YNHdp58msWPk0C75peK2QbOD@localhost:5432/peaksel-shop'
-// export const sequelize = new Sequelize(process.env.POSTGRES_DATABASE || '', process.env.POSTGRES_USER || '',
-//     process.env.POSTGRES_PASSWORD || '',
-//     {
-//         dialect: 'postgres',
-//         // port: 5432,
-//         host: process.env.POSTGRES_HOST,
-//         dialectModule: pg
-//     }
-// )
-export const sequelize = new Sequelize((process.env.POSTGRES_URL || '')+'?sslmode=require')
+export const sequelize = new Sequelize(process.env.POSTGRES_DATABASE || '', process.env.POSTGRES_USER || '',
+    process.env.POSTGRES_PASSWORD || '',
+    {
+        dialect: 'postgres',
+        port: 5432,
+        host: process.env.POSTGRES_HOST,
+        dialectModule: pg,
+        ssl: true,
+    }
+)
+// export const sequelize = new Sequelize((process.env.POSTGRES_URL || '')+'?sslmode=require')
